225de5d2cb120795ac5ed24b813ba9e754966c72
unknown <fitsumtadele19@gmail.com>
2022-05-31 15:48:55 +0300

    slider image fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,11 +9,11 @@ export class HomeComponent implements OnInit {
   title = 'ngSlick';
 
   slides=[
-    {img:"https://m.media-amazon.com/images/S/aplus-media/vc/1b3e3488-ce93-4193-91eb-ff4516810564.__CR0,0,1464,600_PT0_SX1464_V1___.jpg",
-img1:"https://media3.bosch-home.com/Images/600x/MCIM02874659_teaser4_revised1.jpg",
+    {img:"../../assets/tenoch.jpeg",
+img1:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2Ftenacookingoil%2F&psig=AOvVaw3TS9jPwRuk96VGd2aNjf52&ust=1654086399865000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCOj7kZbeifgCFQAAAAAdAAAAABAD",
 urlto:"",
 subtxt:""},
-    {img:"https://www.ttkprestige.com/media/images/html/clip-on-banner.jpg",
+    {img:"../../assets/yes.jpg",
     img1:"https://storage.sg.content-cdn.io/in-resources/fc93a3a8-f69b-444c-8b76-9848de9338d0/Images/userimages/mob-banner/pc/PC-Banner(main).jpg",
   urlto:"",
 subtxt:""},
